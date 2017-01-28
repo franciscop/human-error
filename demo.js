25787aada020b6b95fe811b3a760c914b72f11d3
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-01-28 01:24:35 +0100

    Added demo

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
+// Throw a built-in error quickly
+// run with `node demo.js` and see the error in the terminal
 const errors = require('./human-error')({
   url: key => `https://example.com/error/${key}`
 });
 
-errors.One = () => '';
-errors.Two = () => '';
 errors.Three();
